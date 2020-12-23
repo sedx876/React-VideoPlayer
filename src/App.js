fdff8d941bf6324c87a798e8317d33ed4ff4d9d0
sedx876 <swatkins876@gmail.com>
2020-12-23 07:41:23 -0500

    app.js update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,71 @@
-import React from 'react'
+import React, { Component } from 'react'
+import SocialVideo from './SocialVideo'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Video Player</h1>
-    </div>
-  );
+const videos = [
+  {
+    service: 'vimeo',
+    video: 'https://vimeo.com/121657705'
+  },
+  {
+    service: 'facebook',
+    video: 'https://www.facebook.com/LillysCry/videos/2063699613901421/?t=0'
+  },
+  {
+    service: 'youtube',
+    video: 'https://www.youtube.com/watch?v=5_Te_sazTUY'
+  },
+]
+
+
+class App extends Component {
+  
+  constructor (props){
+    super(props);
+    this.state = {
+      videoIndex: 0
+    };
+  }
+
+  goToVideo (index){
+    let videoIndex = index;
+    if (videoIndex < 0){
+      videoIndex = videos.length - 1;
+    }else if (videoIndex >= videos.length){
+      videoIndex = 0;
+    }
+    this.setState({
+      videoIndex
+    });
+  }
+
+  render(){
+    const {service, video} = videos[this.state.videoIndex];
+    return(
+      <div>
+        <SocialVideo 
+          service={service} 
+          video={video} 
+          width={500} 
+          height={270}
+        />
+
+        <p>
+          <span>{service}: </span>
+          <span>{video}</span>
+        </p>
+
+        <button onClick={this.goToVideo.bind(this, this.state.videoIndex - 1)}>
+          Previous
+        </button>
+
+        <button onClick={this.goToVideo.bind(this, this.state.videoIndex + 1)}>
+          Next
+        </button>
+      </div>
+    )
+  }
+
 }
 
 export default App;
---
+++ src/SocialVideo.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+
+class SocialVideo extends Component {
+  render(){
+    return (
+      <h1>Social Video</h1>
+    )
+  }
+}
+
+export default SocialVideo
